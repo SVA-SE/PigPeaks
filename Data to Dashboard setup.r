f75d55d3b439919c25a2683c0ea001a9f21765c0
Fernanda Dorea <dorea.meyer@gmail.com>
2021-07-19 21:07:38 +0200

    keep track of the setup to run the entire workflow

---
+++ Data to Dashboard setup.r
@@ -0,0 +1,14 @@
+##make sure you are in agreement with
+##Definitions.r
+
+##input farm settings in
+##Settings.r
+
+### RETROSPECTIVE SETUP: -----
+
+## if direct access to database:
+#source("R/1-extract data from database.r")
+
+##only need to do once and then indicators will be saved for all the retrospective phase:
+#source("R/2-create raw data arrays.r")
+
