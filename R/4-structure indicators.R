a2974819fbde362fb6d25d71afdb204905f69664
Fernanda Dorea <dorea.meyer@gmail.com>
2021-08-12 20:40:44 +0200

    carolina's edits, uncomitted because of my conflict.recomitted now

--- R/4-structure indicators.R
+++ R/4-structure indicators.R
@@ -34,39 +34,142 @@ load("data/indicators.RData")
 
 ## Reservices per week
 
-matrix1 <- weekly.indicators(indicator1)
-matrix2 <- weekly.indicators(indicator2)
+range_weekly <- range.weekly(indicator=reservices.week,
+                             weekly.window = 271)
 
+df.reservices.week <- weekly.indicators(indicator=reservices.week)
 
-weekly.indicators(indicators.data=list(reservices.week=reservices.week,
-                                                   number.deaths.week=number.deaths.week,
-                                                   piglets.deaths.week=piglets.deaths.week),
-                              weekly.window=weekly.window)
+df.reservices.week <- clean_baseline_perc(df.indicator=df.reservices.week,
+                                          limit.upp=0.95,
+                                          limit.lw=NULL,
+                                          run.window.weekly=104,
+                                          run.window.continuous=NULL)
 
-weekly.indicators(weekly.window = 300)
+df.reservices.week <- apply_ewma(df.indicator=df.reservices.week,
+                                 evaluate.weekly.window=165,
+                                 baseline.weekly.window=104,
+                                 lambda=0.2,
+                                 limit.sd=c(2.5,3,3.5),
+                                 guard.band.weekly=2,
+                                 correct.baseline.UCL=TRUE,
+                                 correct.baseline.LCL=TRUE,
+                                 UCL=2,
+                                 LCL=2)
 
-
-weekly.indicators.parity(weekly.window=271)
-weekly.indicators.parity(weekly.window=271)[[1]]
+df.reservices.week <- shew_apply(df.indicator=df.reservices.week,
+                                 evaluate.weekly.window=165,
+                                 baseline.weekly.window=104,
+                                 limit.sd=c(2.5,3,3.5),
+                                 guard.band.weekly=2,
+                                 correct.baseline.UCL=FALSE,
+                                 correct.baseline.LCL=FALSE,
+                                 UCL=FALSE,                     
+                                 LCL=FALSE)
 
 
 # Farrowings ----
 
 ## Days between farrowings
 
-continuous.indicators(continuous.window=5500)
+df.days.between.farrowings <- continuous.indicators(indicator=days.between.farrowings,
+                                                    continuous.window=5500)
 
+df.days.between.farrowings <- clean_baseline_perc(df.indicator=df.days.between.farrowings,
+                                                  limit.upp=0.95,
+                                                  limit.lw=0.05,
+                                                  run.window.weekly=NULL,
+                                                  run.window.continuous=1300)
+
+df.days.between.farrowings <- apply_ewma(df.indicator=df.days.between.farrowings,
+                                         evaluate.weekly.window=NULL,
+                                         baseline.weekly.window=NULL,
+                                         lambda=0.2,
+                                         limit.sd=c(2.5,3,3.5),
+                                         guard.band.weekly=NULL,
+                                         correct.baseline.UCL=TRUE,
+                                         correct.baseline.LCL=TRUE,
+                                         UCL=2,
+                                         LCL=2)
+
+df.days.between.farrowings <- shew_apply(df.indicator=df.indicator,
+                                         evaluate.weekly.window=NULL,
+                                         baseline.weekly.window=NULL,
+                                         limit.sd=c(2.5,3,3.5),
+                                         guard.band.weekly=NULL,
+                                         correct.baseline.UCL=FALSE,
+                                         correct.baseline.LCL=FALSE,
+                                         UCL=FALSE,                     
+                                         LCL=FALSE)
 
 # Post-Weaning ----
 
 ## Deaths in weaners per week
 
-weekly.indicators.nonparity(weekly.window=271)
+range_weekly <- range.weekly(indicator=piglets.deaths.week,
+                             weekly.window = 271)
+
+df.piglets.deaths.week <- weekly.indicators(indicator=piglets.deaths.week)
+
+df.piglets.deaths.week <- clean_baseline_perc(df.indicator=df.piglets.deaths.week,
+                                              limit.upp=0.95,
+                                              limit.lw=NULL,
+                                              run.window.weekly=104,
+                                              run.window.continuous=NULL)
+
+df.piglets.deaths.week <- apply_ewma(df.indicator=df.piglets.deaths.week,
+                                     evaluate.weekly.window=165,
+                                     baseline.weekly.window=104,
+                                     lambda=0.2,
+                                     limit.sd=c(2.5,3,3.5),
+                                     guard.band.weekly=2,
+                                     correct.baseline.UCL=TRUE,
+                                     correct.baseline.LCL=TRUE,
+                                     UCL=2,
+                                     LCL=2)
+
+df.piglets.deaths.week <- shew_apply(df.indicator=df.piglets.deaths.week,
+                                     evaluate.weekly.window=165,
+                                     baseline.weekly.window=104,
+                                     limit.sd=c(2.5,3,3.5),
+                                     guard.band.weekly=2,
+                                     correct.baseline.UCL=FALSE,
+                                     correct.baseline.LCL=FALSE,
+                                     UCL=FALSE,                     
+                                     LCL=FALSE)
 
 
 # Exit ----
 
 ## Dead sows per week
 
-weekly.indicators.parity(weekly.window=271)
-weekly.indicators.parity(weekly.window=271)[[2]]
+range_weekly <- range.weekly(indicator=number.deaths.week,
+                             weekly.window = 271)
+
+df.number.deaths.week <- weekly.indicators(indicator=number.deaths.week)
+
+df.number.deaths.week <- clean_baseline_perc(df.indicator=df.number.deaths.week,
+                                             limit.upp=0.95,
+                                             limit.lw=NULL,
+                                             run.window.weekly=104,
+                                             run.window.continuous=NULL)
+
+df.number.deaths.week <- apply_ewma(df.indicator=df.number.deaths.week,
+                                    evaluate.weekly.window=165,
+                                    baseline.weekly.window=104,
+                                    lambda=0.2,
+                                    limit.sd=c(2.5,3,3.5),
+                                    guard.band.weekly=2,
+                                    correct.baseline.UCL=TRUE,
+                                    correct.baseline.LCL=TRUE,
+                                    UCL=2,
+                                    LCL=2)
+
+df.number.deaths.week <- shew_apply(df.indicator=df.number.deaths.week,
+                                    evaluate.weekly.window=165,
+                                    baseline.weekly.window=104,
+                                    limit.sd=c(2.5,3,3.5),
+                                    guard.band.weekly=2,
+                                    correct.baseline.UCL=FALSE,
+                                    correct.baseline.LCL=FALSE,
+                                    UCL=FALSE,                     
+                                    LCL=FALSE)
