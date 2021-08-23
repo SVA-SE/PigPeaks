bf9e4dbb818002bd72148abab47b3fd9c8efe2b7
carolina Merca <carolinamerka@gmail.com>
2021-08-23 17:16:55 +0100

    remove plotting fuctions

--- R/Functions.r
+++ R/Functions.r
@@ -1,10 +1,9 @@
-packages <- c("ISOweek","RColorBrewer", "caTools", "lubridate", "abind", "qcc", "dplyr")
+packages <- c("ISOweek", "caTools", "lubridate", "abind", "qcc", "dplyr")
 install.packages(setdiff(packages, rownames(installed.packages())))
 
 require(ISOweek)
-require(RColorBrewer)
-require(lubridate)
 require(caTools)
+require(lubridate)
 require(abind)
 require(qcc)
 require(dplyr)
@@ -943,32 +942,6 @@ shew_apply <- function (df.indicator=df.indicator,
   
   return(df.indicator)
   }
-
-# plotting functions ----
-
-
-# parity colouring
-#parity.group <- data.frame(parity = c1, group=c2, group.name = c3)
-parity.group <- data.frame(parity = c1, group.name = ordered(c3,levels=c("gilt","young","prime","mature")))
-
-
-qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
-col_parity = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
-
-parity.group$color1 <- col_parity[as.numeric(as.factor(parity.group$group.name))]
-parity.group$color2 <- col_parity[as.numeric(as.factor(parity.group$parity))]
-
-parity.group2 <- parity.group[-1,]
-
-colors.custom<- c(rep("#4287f5",1),
-                  rep("#28ab1f",2),
-                  rep("#f5942c",3),
-                  rep("#a15a4c",9)
-)
-parity.group2 <-cbind(parity.group2,colors.custom)
-parity.group2$colors.custom<-colors.custom
-
-color.pg <- c("#4287f5","#28ab1f","#f5942c","#a15a4c")
 
 
 
