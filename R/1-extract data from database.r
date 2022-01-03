01c8f66988aed033f79c298623c642a9bc4ce30a
Nanda Dórea <dorea.meyer@gmail.com>
2022-01-03 10:59:03 +0100

    date filter correction

--- R/1-extract data from database.r
+++ R/1-extract data from database.r
@@ -21,21 +21,21 @@ date_logical <- "<= "
 
 
 animal              <- sqlQuery(conn, "SELECT ID,AnimalNumber,BirthDate,Sex,AnimalType,EntryDate,ExitDate,ExitType,ExitCause1Id,ExitCause2Id  FROM animal;")
-service             <- sqlQuery(conn, paste0("SELECT * FROM service WHERE EventDate ",date_logical,"'",install.date,"' ;"))
+service             <- sqlQuery(conn, paste0("SELECT * FROM service WHERE EventDate ",date_logical,"'",date_filter,"' ;"))
 service <- service[service$LinkedToEvent!=1,]
-pregnancyTest      <- sqlQuery(conn, paste0("SELECT * FROM pregnancyTest WHERE EventDate ",date_logical,"'",install.date,"' ;"))
-abortion            <- sqlQuery(conn, paste0("SELECT * FROM abortion WHERE EventDate ",date_logical,"'",install.date,"' ;"))
-farrowing           <- sqlQuery(conn, paste0("SELECT * FROM farrowing WHERE EventDate ",date_logical,"'",install.date,"' ;"))
-weaning             <- sqlQuery(conn, paste0("SELECT * FROM weaning WHERE EventDate ",date_logical,"'",install.date,"' ;"))
+pregnancyTest      <- sqlQuery(conn, paste0("SELECT * FROM pregnancyTest WHERE EventDate ",date_logical,"'",date_filter,"' ;"))
+abortion            <- sqlQuery(conn, paste0("SELECT * FROM abortion WHERE EventDate ",date_logical,"'",date_filter,"' ;"))
+farrowing           <- sqlQuery(conn, paste0("SELECT * FROM farrowing WHERE EventDate ",date_logical,"'",date_filter,"' ;"))
+weaning             <- sqlQuery(conn, paste0("SELECT * FROM weaning WHERE EventDate ",date_logical,"'",date_filter,"' ;"))
 
-dead.piglet         <- sqlQuery(conn, paste0("SELECT * FROM deadPiglet WHERE EventDate ",date_logical,"'",install.date,"' ;"))
-dead.piglet.group   <- sqlQuery(conn, paste0("SELECT * FROM deadPigletGroup WHERE EventDate ",date_logical,"'",install.date,"' ;"))
+dead.piglet         <- sqlQuery(conn, paste0("SELECT * FROM deadPiglet WHERE EventDate ",date_logical,"'",date_filter,"' ;"))
+dead.piglet.group   <- sqlQuery(conn, paste0("SELECT * FROM deadPigletGroup WHERE EventDate ",date_logical,"'",date_filter,"' ;"))
 
-progeny.count       <- sqlQuery(conn, paste0("SELECT * FROM progenyCount WHERE EventDate ",date_logical,"'",install.date,"' ;"))
-progeny.dead        <- sqlQuery(conn, paste0("SELECT * FROM progenyDead WHERE EventDate ",date_logical,"'",install.date,"' ;"))
-progeny.entry       <- sqlQuery(conn, paste0("SELECT * FROM progenyEntry WHERE EventDate ",date_logical,"'",install.date,"' ;"))
-progeny.exit        <- sqlQuery(conn, paste0("SELECT * FROM progenyExit WHERE EventDate ",date_logical,"'",install.date,"' ;"))
-progeny.transfer    <- sqlQuery(conn, paste0("SELECT * FROM progenyTransfer WHERE EventDate ",date_logical,"'",install.date,"' ;"))
+progeny.count       <- sqlQuery(conn, paste0("SELECT * FROM progenyCount WHERE EventDate ",date_logical,"'",date_filter,"' ;"))
+progeny.dead        <- sqlQuery(conn, paste0("SELECT * FROM progenyDead WHERE EventDate ",date_logical,"'",date_filter,"' ;"))
+progeny.entry       <- sqlQuery(conn, paste0("SELECT * FROM progenyEntry WHERE EventDate ",date_logical,"'",date_filter,"' ;"))
+progeny.exit        <- sqlQuery(conn, paste0("SELECT * FROM progenyExit WHERE EventDate ",date_logical,"'",date_filter,"' ;"))
+progeny.transfer    <- sqlQuery(conn, paste0("SELECT * FROM progenyTransfer WHERE EventDate ",date_logical,"'",date_filter,"' ;"))
 cause               <- sqlQuery(conn, "SELECT * FROM Cause_NameTranslated;")
 
 rm(conn,database,packages,server)
