8632a1abb2712cfada99c8786c3bfd354f176c0d
Wiktor Gustafsson <wiktor.gustafsson@sva.se>
2024-03-26 14:52:27 +0100

    Fix bug

--- R/2-create raw data arrays.r
+++ R/2-create raw data arrays.r
@@ -486,7 +486,7 @@ for (s in 1:dim(individual.sows[[1]])[2]){
 
 
 save(individual.sows,active.sows.displayID,index.dates.days,file="data/individual.sows.RData")
-save(animal,exit,progeny.dead,cause,file="data/animal.RData")
+save(animal,exit,progeny.dead,file="data/animal.RData")
 
 
 
